cc2d7c41b4183376f5af2c50888f457013747582
ZichongHuang <43491012+ZichongHuang@users.noreply.github.com>
2018-09-23 16:56:54 +0800

    add file

---
+++ queue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stackApp
+{
+    class queue
+    {
+        private object[] queueArray;
+        private int frontIndex;
+        private int rearIndex;
+
+        public queue(int length)
+        {
+            queueArray = new object[length];
+            frontIndex = 0;
+            rearIndex = 0;
+        }
+
+        private int moveFlag(int index,int move)
+        {
+            int tempIndex;
+            tempIndex = index + move;
+            if(tempIndex>=queueArray.Length)
+            {
+                tempIndex = tempIndex - queueArray.Length;
+            }
+            else if(tempIndex<=0)
+            {
+                tempIndex = tempIndex + queueArray.Length;
+            }
+            return tempIndex;
+        }
+
+        public int itemIn(object item)
+        {
+            if (queueArray[rearIndex]==null)
+            {
+                queueArray[rearIndex] = item;
+                rearIndex=moveFlag(rearIndex,1);
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public object itemOut()
+        {
+            if(!(queueArray[frontIndex]==null))
+            {
+                object item;
+                item = queueArray[frontIndex];
+                queueArray[frontIndex] = null;
+                frontIndex = moveFlag(frontIndex, 1);
+                return item;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ stack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stackApp
+{
+    class stack
+    {
+        private ArrayList stackArray;
+
+        public stack(int mostLength)
+        {
+            stackArray = new ArrayList();
+            stackArray.Capacity = mostLength;
+        }
+
+        public int itemIn(object item)
+        {
+            if (stackArray.Count < stackArray.Capacity)
+            {
+                stackArray.Add(item);
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int itemForceIn(object item)
+        {
+            if (stackArray.Count < stackArray.Capacity)
+            {
+                stackArray.Add(item);
+                return 0;
+            }
+            else
+            {
+                stackArray.Capacity++;
+                stackArray.Add(item);
+                return -1;
+            }
+        }
+
+        public object itemOut()
+        {
+            if (stackArray.Count != 0)
+            {
+                object temp = stackArray[stackArray.Count - 1];
+                stackArray.RemoveAt(stackArray.Count - 1);
+                return temp;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int count()
+        {
+            return stackArray.Count;
+        }
+
+        public int changeLength(int length)
+        {
+            if(length>stackArray.Count)
+            {
+                stackArray.Capacity = length;
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+}
