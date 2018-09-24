1b333ac74c489787865fa70e41ae71a1f61eaf75
ZichongHuang <43491012+ZichongHuang@users.noreply.github.com>
2018-09-24 17:16:42 +0800

    添加树的代码

---
+++ node.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stackApp
+{
+    class node
+    {
+        private string name;
+        private int layerNum;
+        private object content;
+        private List<node> childs;
+        private node parent;
+
+        /// <summary>
+        /// 创建一个节点
+        /// </summary>
+        /// <param name="name">节点名</param>
+        public node(string name)
+        {
+            this.name = name;
+            layerNum = -1;
+            childs = new List<node>();
+        }
+
+        /// <summary>
+        /// 重命名节点
+        /// </summary>
+        /// <param name="name">节点名</param>
+        public void resetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string readName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// 设置节点所在层数
+        /// </summary>
+        /// <param name="layerNum">层数</param>
+        public void setLayerNum(int layerNum)
+        {
+            this.layerNum = layerNum;
+        }
+
+        /// <summary>
+        /// 设置父节点
+        /// </summary>
+        /// <param name="parent">父节点</param>
+        public void setParent(node parent)
+        {
+            this.parent = parent;
+        }
+
+        /// <summary>
+        /// 获取父节点
+        /// </summary>
+        /// <returns>父节点</returns>
+        public node getParent()
+        {
+            return parent;
+        }
+        /// <summary>
+        /// 读取节点所在层数
+        /// </summary>
+        /// <returns>层数</returns>
+        public int readLayerNum()
+        {        
+                return layerNum;
+        }
+
+        /// <summary>
+        /// 向节点添加内容
+        /// </summary>
+        /// <param name="content">内容类</param>
+        public void addContent(object content)
+        {
+            this.content = content;
+        }
+
+        /// <summary>
+        /// 向节点添加子节点
+        /// </summary>
+        /// <param name="child">子节点</param>
+        /// <returns>重复状态</returns>
+        public int addChildNode(node child)
+        {
+            if (childs.Contains(child))
+            {
+                return -1;
+            }
+            else
+            {
+                this.childs.Add(child);
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// 从节点删除子节点
+        /// </summary>
+        /// <param name="child">子节点</param>
+        public void removeChildNode(node child)
+        {
+            this.childs.Remove(child);
+        }
+
+        /// <summary>
+        /// 获取所有子节点
+        /// </summary>
+        /// <returns>所有子节点</returns>
+        public List<node> getChildNode()
+        {
+            return childs;
+        }
+
+        /// <summary>
+        /// 删除这个节点下的所有子节点
+        /// 从树表中删除这个节点及其子节点
+        /// </summary>
+        /// <param name="tree">树</param>
+        public void delete(tree tree)
+        {
+            tree.deleteNodeInTreeList(this);
+            //从树表中将自己删除
+            foreach (node child in childs)
+            {
+                child.delete(tree);
+            }
+            //调用子节点的删除方法
+            for (int i = 0; i < childs.Count; i++)
+            {
+                childs[i] = null;
+            }
+            //释放所有子节点的实例
+            childs.Clear();
+            //从表中删除所有子节点
+        }
+
+    }
+}
---
+++ tree.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stackApp
+{
+    class tree
+    {
+        private List<List<node>> treeList;
+
+        /// <summary>
+        /// 创建一颗树并自动添加一个根节点
+        /// </summary>
+        public tree()
+        {
+            treeList = new List<List<node>>();
+            List<node> layer = new List<node>();
+            treeList.Add(layer);
+            node rootNode = new node("root");
+            rootNode.setLayerNum(0);
+            treeList[0].Add(rootNode);
+        }
+
+        /// <summary>
+        /// 在树表中添加一个节点
+        /// </summary>
+        /// <param name="node">节点</param>
+        /// <param name="layerNum">层数</param>
+        private void addNode2Layer(node node)
+        {
+            int layerNum = node.readLayerNum();
+            if(treeList.Count<=layerNum)
+            {
+                //第一次到达该层
+                treeList.Add(new List<node>());
+                //新建一个层
+                treeList[layerNum].Add(node);
+                //向层中添加节点
+            }
+            else
+            {
+                //已经有该层
+                treeList[layerNum].Add(node);
+            }
+        }
+
+        /// <summary>
+        /// 创建并添加一个节点
+        /// </summary>
+        /// <param name="parent">父节点</param>
+        /// <param name="name">节点名</param>
+        /// <returns>新节点</returns>
+        public node addNode(node parent,string name)
+        {
+            int layerNum = parent.readLayerNum() + 1;
+            //获得这个节点的层数
+            node node = new node(name);
+            //创建节点
+            node.setLayerNum(layerNum);
+            //设置层数
+            node.setParent(parent);
+            //添加父节点
+            parent.addChildNode(node);
+            //向父节点添加
+            addNode2Layer(node);
+            //向树表添加
+            return node;
+        }
+
+        /// <summary>
+        /// 在整颗树中查找节点
+        /// </summary>
+        /// <param name="name">节点名</param>
+        /// <returns>节点</returns>
+        public node findNode(string name)
+        {
+            foreach (List<node> layer in treeList)
+            {
+                foreach (node node in layer)
+                {
+                    if(node.readName()==name)
+                    {
+                        return node;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 在指定层查找结点
+        /// </summary>
+        /// <param name="name">节点名</param>
+        /// <param name="layerNum">层数</param>
+        /// <returns>节点</returns>
+        public node findNode(string name,int layerNum)
+        {
+            if (layerNum < treeList.Count)
+            {
+                foreach (node node in treeList[layerNum])
+                {
+                    if (node.readName() == name)
+                    {
+                        return node;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 在指定区间查找结点
+        /// </summary>
+        /// <param name="name">节点名</param>
+        /// <param name="minLayer">起点层数</param>
+        /// <param name="maxLayer">终点层数</param>
+        /// <returns>节点</returns>
+        public node findNode(string name,int minLayer,int maxLayer)
+        {
+            if ((maxLayer < treeList.Count)&&(minLayer>0))
+            {
+                for (int i = minLayer; i <= maxLayer; i++)
+                {
+                    List<node> layer = treeList[i];
+                    foreach (node node in layer)
+                    {
+                        if (node.readName() == name)
+                        {
+                            return node;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 从树表中删除节点
+        /// </summary>
+        /// <param name="node">节点</param>
+        /// <param name="layerNum">层数</param>
+        public void deleteNodeInTreeList(node node)
+        {
+            int layerNum = node.readLayerNum();
+            treeList[layerNum].Remove(node);
+        }
+
+        /// <summary>
+        /// 彻底删除节点及其所有子节点
+        /// </summary>
+        /// <param name="node">节点</param>
+        /// <param name="layerNum">层数</param>
+        public void deleteNode(node node)
+        {
+            node.delete(this);
+            node.getParent().removeChildNode(node);
+            node = null;
+        }
+
+        /// <summary>
+        /// 输出树结构
+        /// </summary>
+        /// <returns>包含树结构的字符串</returns>
+        public string printTree()
+        {
+            StringBuilder treeNode=new StringBuilder(20);
+            foreach (List<node> layer in treeList)
+            {
+                foreach (node node in layer)
+                {
+                    treeNode.Append(node.readName());
+                    treeNode.Append("  ");
+                }
+                //层读取结束
+                treeNode.Append("\n");
+            }
+
+            return treeNode.ToString();
+        }
+
+    }
+}
